19b10d8933773407a6e00582d6e991bf469ab163
tangqiushi <1056163724@qq.com>
2019-04-25 20:03:04 +0800

    1

--- src/githubtest/Demo1.java
+++ src/githubtest/Demo1.java
@@ -4,11 +4,7 @@ public class Demo1 {
 	public static void main(String[] args) {
 		String s = "test1";
 		System.out.println(s);
-<<<<<<< HEAD
-		String s2 = "µÚ¶þ´ÎÌá½»";
-=======
-		String s2 = "ç¬¬ä¸€æ¬¡ä¿®æ”¹";
->>>>>>> branch 'master' of https://github.com/tangqiushi/githubtest.git
+		String s2 = "ç¬¬ä¸€æ¬¡ä¿®æ”?;
 	}
 
 }
